ab07769a0ce614ae34054f5a04556d32e0ff21ef
Alexander Mykolaichuk <alexmix3000@gmail.com>
2017-08-20 12:40:34 +0300

    info item header styles

--- App/Containers/HOC/withHeaderWrapper.js
+++ App/Containers/HOC/withHeaderWrapper.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { Platform, View } from 'react-native';
 import { Header } from 'react-navigation';
 
 const withHeaderWrapper = Component => class extends React.Component {
   static navigationOptions = ({ navigation }) => ({
     title: navigation.state.params.data.title,
-    header: props => <Header {...props} />,
+    headerStyle: {
+      backgroundColor: '#00B47F',
+      elevation: 0,
+    },
+    headerTitleStyle: {
+      color: '#fff',
+    },
+    headerTintColor: '#fff',
+    header: props => (
+      <View>
+        {Platform.OS === 'android' && Platform.Version >= 20
+          ? <View
+            style={{
+              height: 24,
+              backgroundColor: '#00B47F',
+            }}
+          />
+          : null}
+        <Header {...props} />
+      </View>
+    ),
   });
 
   render() {
