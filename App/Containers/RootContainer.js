84df94fa8400cb1aa160ade12262040164bb3137
Alexander Mykolaichuk <alexmix3000@gmail.com>
2017-08-20 11:50:12 +0300

    add status bar styles

--- App/Containers/RootContainer.js
+++ App/Containers/RootContainer.js
@@ -1,34 +1,34 @@
-import React, { Component } from 'react'
-import { View, StatusBar } from 'react-native'
-import ReduxNavigation from '../Navigation/ReduxNavigation'
-import { connect } from 'react-redux'
-import StartupActions from '../Redux/StartupRedux'
-import ReduxPersist from '../Config/ReduxPersist'
+import React, { Component } from 'react';
+import { View, StatusBar } from 'react-native';
+import { connect } from 'react-redux';
+import ReduxNavigation from '../Navigation/ReduxNavigation';
+import StartupActions from '../Redux/StartupRedux';
+import ReduxPersist from '../Config/ReduxPersist';
 
 // Styles
-import styles from './Styles/RootContainerStyles'
+import styles from './Styles/RootContainerStyles';
 
 class RootContainer extends Component {
-  componentDidMount () {
+  componentDidMount() {
     // if redux persist is not active fire startup action
     if (!ReduxPersist.active) {
-      this.props.startup()
+      this.props.startup();
     }
   }
 
-  render () {
+  render() {
     return (
       <View style={styles.applicationView}>
-        <StatusBar backgoundColor="#00b47f" barStyle='light-content' />
+        <StatusBar translucent backgroundColor="rgba(0, 0, 0, 0.20)" animated />
         <ReduxNavigation />
       </View>
-    )
+    );
   }
 }
 
 // wraps dispatch to create nicer functions to call within our component
-const mapDispatchToProps = (dispatch) => ({
-  startup: () => dispatch(StartupActions.startup())
-})
+const mapDispatchToProps = dispatch => ({
+  startup: () => dispatch(StartupActions.startup()),
+});
 
-export default connect(null, mapDispatchToProps)(RootContainer)
+export default connect(null, mapDispatchToProps)(RootContainer);
--- App/Navigation/HeaderBarSettings/navBarSettings.js
+++ App/Navigation/HeaderBarSettings/navBarSettings.js
@@ -1,24 +1,40 @@
-import React from 'react'
-import NavBarItem from './NavBarItem'
+import React from 'react';
+import { View, Platform } from 'react-native';
+import { Header } from 'react-navigation';
+import NavBarItem from './NavBarItem';
 
 export const getNavigationOptionsWithAction = (backgroundColor, color, headerLeft) => ({
   headerStyle: {
-    backgroundColor
+    backgroundColor,
+    elevation: 0,
   },
   headerTitleStyle: {
     color,
-    fontWeight: '500'
+    fontWeight: '500',
   },
   headerTintColor: color,
-  headerLeft
-})
+  headerLeft,
+  header: props => (
+    <View>
+      {Platform.OS === 'android' && Platform.Version >= 20
+        ? <View
+          style={{
+            height: 24,
+            backgroundColor: '#00B47F',
+          }}
+        />
+        : null}
+      <Header {...props} />
+    </View>
+  ),
+});
 
 export const getNavBarIcon = (navigation, color) => (
   <NavBarItem
-    iconName='bars'
+    iconName="bars"
     color={color}
     onPress={() => {
-      navigation.navigate('DrawerOpen')
+      navigation.navigate('DrawerOpen');
     }}
   />
-)
+);
